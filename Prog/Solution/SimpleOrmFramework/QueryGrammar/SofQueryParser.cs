22ce1ce7dc7eaf40f81770f4f081018ba934ee79
cepnox <cepnox@localhost>
2010-12-19 21:12:26 +0100

    Added copyright

--- Prog/Solution/SimpleOrmFramework/QueryGrammar/SofQueryParser.cs
+++ Prog/Solution/SimpleOrmFramework/QueryGrammar/SofQueryParser.cs
@@ -1,3 +1,21 @@
+//////////////////////////////////////////////////////////////////////
+//  Copyright (C) 2010 by Etrak Studios <etrakstudios@gmail.com >
+//  Part of the Conquera Project
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+////////////////////////////////////////////////////////////////////////
+
 // $ANTLR 3.1.2 D:\\Prog\\Projects\\AL\\Prog\\Solution\\SimpleOrmFramework\\QueryGrammar\\SofQuery.g 2009-12-23 12:47:48
 
 // The variable 'variable' is assigned but its value is never used.
