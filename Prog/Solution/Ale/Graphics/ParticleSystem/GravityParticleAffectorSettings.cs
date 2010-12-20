34a00422bd38f79879c1d81050cf699e3ef9f73f
cepnox <pruttned@gmail.com>
2010-12-20 19:50:32 +0100

    license

--- Prog/Solution/Ale/Graphics/ParticleSystem/GravityParticleAffectorSettings.cs
+++ Prog/Solution/Ale/Graphics/ParticleSystem/GravityParticleAffectorSettings.cs
@@ -1,5 +1,5 @@
 //////////////////////////////////////////////////////////////////////
-//  Copyright (C) 2010 by Etrak Studios <etrakstudios@gmail.com >
+//  Copyright (C) 2010 by Conquera Team
 //  Part of the Conquera Project
 //
 //  This program is free software: you can redistribute it and/or modify
