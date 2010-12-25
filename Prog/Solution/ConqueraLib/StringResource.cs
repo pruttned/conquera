501f3785e410b31f78d90e9fdd642f0769c95e3a
cepnox <pruttned@gmail.com>
2010-12-25 15:34:54 +0100

    Added missing file

---
+++ Prog/Solution/ConqueraLib/StringResource.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SimpleOrmFramework;
+
+namespace Conquera
+{
+    [DataObject]
+    public class StringResource : BaseDataObject
+    {
+        [DataProperty(NotNull=true, Unique=true)]
+        public string Name { get; private set; }
+        [DataProperty(NotNull = true)]
+        public string Text { get; private set; }
+
+        public StringResource(string name, string text)
+        {
+            Name = name;
+            Text = text;
+        }
+
+        private StringResource()
+        {
+        }
+    }
+}
