bffcf63540b43c3bf3ab8d8b1aaf5e7da731feb9
cepnox <cepnox@localhost>
2010-12-27 19:26:38 +0100

    Spell - BeforeAttack

--- Prog/Solution/Conquera/Program.cs
+++ Prog/Solution/Conquera/Program.cs
@@ -646,6 +646,39 @@ namespace Conquera
                     //    ormManager.SaveObject(particleSystemSettings);
                     //}
 
+
+                    //{
+                    //    ParticleSystemSettings particleSystemSettings = new ParticleSystemSettings();
+                    //    particleSystemSettings.Name = "SlayerPsys";
+
+                    //    SphereParticleEmitterSettings particleEmitterSettings = new SphereParticleEmitterSettings();
+                    //    particleSystemSettings.Emitters.Add(particleEmitterSettings);
+
+                    //    particleEmitterSettings.FastForwardTimeOnLoad = 0;
+                    //    particleEmitterSettings.Radius = 0.24f;
+                    //    particleEmitterSettings.MaxParticleCnt = 100;
+                    //    particleEmitterSettings.ParticleLifeDuration = 2f;
+                    //    particleEmitterSettings.ParticleLifeDurationVariation = 0.5f;
+                    //    particleEmitterSettings.RelativePosition = new Vector3(0, 0, 1f);
+                    //    particleEmitterSettings.ParticleDirectionAngles = new Vector2(MathHelper.ToRadians(180), MathHelper.ToRadians(0));
+                    //    particleEmitterSettings.ParticleDirectionVariation = MathHelper.ToRadians(5.0f);
+                    //    particleEmitterSettings.ParticleSettings.ParticleColorRFunction = new TimeFunction(new Vector2(0, 0.8f), new Vector2(0.4f, 0.9f), new Vector2(0.6f, 0.1f), new Vector2(1.0f, 0.2f));
+                    //    particleEmitterSettings.ParticleSettings.ParticleColorGFunction = new TimeFunction(new Vector2(0, 0.1f), new Vector2(0.4f, 0.1f), new Vector2(0.6f, 0.0f), new Vector2(1.0f, 0.8f));
+                    //    particleEmitterSettings.ParticleSettings.ParticleColorBFunction = new TimeFunction(new Vector2(0, 1.0f), new Vector2(0.4f, 0.3f), new Vector2(0.6f, 0.9f), new Vector2(1.0f, 1.0f));
+                    //    particleEmitterSettings.ParticleSettings.ParticleColorAFunction = new TimeFunction(new Vector2(0, 0), new Vector2(0.5f, 0.8f), new Vector2(0.8f, 0.8f), new Vector2(1.0f, 0));
+                    //    particleEmitterSettings.ParticleSettings.ParticleSpeed = new TimeFunction(0.3f, 0.8f);
+                    //    particleEmitterSettings.ParticleSettings.ParticleSize = new TimeFunction(0.3f, 0.1f);
+                    //    particleEmitterSettings.ParticleSettings.ParticleRotation = new TimeFunction(0, 1);
+                    //    particleEmitterSettings.EmissionRateFunction = new TimeFunction(100);
+                    //    particleEmitterSettings.CycleTime = 1f;
+
+                    //    particleEmitterSettings.Material = ormManager.FindObject(typeof(MaterialSettings), "Name='ParticleMaterial'");
+                    //    particleEmitterSettings.Name = "Emitter1";
+
+
+                    //    ormManager.SaveObject(particleSystemSettings);
+                    //}
+
                     transaction.Commit();
                 }
             }
--- Prog/Solution/ContentData/Content/GuiPalettes/CellNotificationPalette.plt
+++ Prog/Solution/ContentData/Content/GuiPalettes/CellNotificationPalette.plt
@@ -3,6 +3,8 @@
     <image name="Coin" textureFileName="CellNotificationText" locationX="0" locationY="0" width="32" height="32"/>
     <image name="Hearth" textureFileName="CellNotificationText" locationX="32" locationY="0" width="32" height="32"/>
     <image name="BrokenHearth" textureFileName="CellNotificationText" locationX="0" locationY="32" width="32" height="32"/>
+    <image name="FireStorm" textureFileName="CellNotificationText" locationX="64" locationY="0" width="64" height="64"/>
+    <image name="Slayer" textureFileName="CellNotificationText" locationX="0" locationY="64" width="64" height="64"/>
   </graphicElements>
 
 </guiPalette>
