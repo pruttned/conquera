1abf58ec1c8763f554007af6d0bf132767bb9966
cepnox <pruttned@gmail.com>
2011-01-09 11:18:59 +0100

    MindControlSpell

--- Prog/Solution/ConqueraLib/Spell/MindControlSpell.cs
+++ Prog/Solution/ConqueraLib/Spell/MindControlSpell.cs
@@ -32,6 +32,9 @@ namespace Conquera
     {
         private static GraphicElement mPictureGraphicElement = GuiManager.Instance.Palette.CreateGraphicElement("SpellIconMindControl");
         private static GraphicElement mIconGraphicElement = GuiManager.Instance.Palette.CreateGraphicElement("SpellIconMindControl");
+        private static float DivCoef = 3;
+
+        private AnimationDelay mAttackDelay = new AnimationDelay();
 
         public override GraphicElement Picture
         {
@@ -60,11 +63,12 @@ namespace Conquera
 
         public override int ApplyAttackModifiers(int baseAttack)
         {
-            return baseAttack;
+            return baseAttack + (int)Math.Ceiling((float)Target.GameUnitDesc.Attack / DivCoef);
         }
 
         protected override void BeforeAttackCastImpl()
         {
+            Target.GameScene.FireCellNotificationLabel("", CellNotificationIcons.MindControl, Color.Red, Target.CellIndex);
         }
 
         protected override bool BeforeAttackUpdateImpl(AleGameTime time)
