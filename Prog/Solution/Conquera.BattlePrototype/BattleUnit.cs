8db8e19625fb0c117b3e00e5b788dc8ad969d82b
cepnox <cepnox@localhost>
2011-10-31 14:36:34 +0100

    missing file

---
+++ Prog/Solution/Conquera.BattlePrototype/BattleUnit.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Conquera.BattlePrototype
+{
+    public class BattleUnit
+    {
+        public delegate void CellIndexChangedHandler(BattleUnit obj, Point oldValue);
+        public event CellIndexChangedHandler TileIndexChanged;
+
+        private Point mTileIndex;
+
+        public Vector2 Position { get; private set; }
+
+        public Point TileIndex
+        {
+            get { return mTileIndex; }
+            set
+            {
+                if (mTileIndex != value)
+                {
+                    throw new NotImplementedException();
+                    //Point oldValue = mTileIndex;
+                    //mTileIndex = value;
+
+                    //UpdatePositionFromIndex();
+
+                    //OnCellIndexChanged(oldValue);
+
+                    //if (null != CellIndexChanged)
+                    //{
+                    //    CellIndexChanged.Invoke(this, oldValue);
+                    //}
+                }
+            }
+        }
+    }
+}
