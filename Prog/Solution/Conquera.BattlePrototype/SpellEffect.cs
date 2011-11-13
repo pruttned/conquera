4dd5cf46fb112198f0bec29ebff2716eb2ad5cc0
cepnox <pruttned@gmail.com>
2011-11-13 16:24:11 +0100

    Missing file

---
+++ Prog/Solution/Conquera.BattlePrototype/SpellEffect.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Conquera.BattlePrototype
+{
+    public interface IBattleUnitSpellEffect
+    {
+        bool OnStartTurn(int turnNum, BattlePlayer playerOnTurn);
+    }
+
+    public interface IBattlePlayerSpellEffect
+    {
+        bool OnStartTurn();
+        bool OnUnitDeath();
+    }
+
+
+    //public class DefenseBattleUnitSpellEffect : IBattleUnitSpellEffect
+    //{
+    //    bool OnStartTurn();
+    //    int GetDefenseModifier();
+    //    int GetAttackModifier();
+    //    int GetMoveDistanceModifier();
+    //}
+
+    public interface IBattleUnitDefenseModifier
+    {
+        int GetModifier(BattleUnit unit);
+    }
+    public interface IBattleUnitAttackModifier
+    {
+        int GetModifier(BattleUnit unit);
+    }
+    public interface IBattleUnitMovementDistanceModifier
+    {
+        int GetModifier(BattleUnit unit);
+    }
+
+}
