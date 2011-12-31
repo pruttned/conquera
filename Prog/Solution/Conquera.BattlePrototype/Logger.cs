d167ed62545fb336a30db0ea64b1ee1a7f6e5872
Mok <Mok@localhost>
2011-12-31 12:32:42 +0100

    BattlePrototype - logger added

---
+++ Prog/Solution/Conquera.BattlePrototype/Logger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace Conquera.BattlePrototype
+{
+    public static class Logger
+    {
+        internal class LogEventArgs : EventArgs
+        {
+            public object Record;
+            public ListBoxItem Item;
+        }
+
+        public delegate void RecordsDoubleClickHandler(object record);
+
+        internal static event EventHandler<LogEventArgs> Logged;
+
+        public static void Log(object record)
+        {
+            Log(record, null);
+        }
+
+        public static void Log(object record, RecordsDoubleClickHandler onDoubleClick)
+        {
+            if (Logged != null)
+            {
+                LogEventArgs args = new LogEventArgs { Record = record };
+                Logged(null, args);
+
+                if (onDoubleClick != null)
+                {
+                    args.Item.MouseDoubleClick += (sender, e) => onDoubleClick(record);
+                }
+            }
+        }
+    }
+}
